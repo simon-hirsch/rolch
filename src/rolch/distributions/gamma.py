d461f5197c72984fcf6743b5d3e7bd940f88e966
simon-hirsch <simon.hirsch@stud.uni-due.de>
2024-08-29 10:16:01 +0200

    Add parameter mapping scipy - gamlss

--- src/rolch/distributions/gamma.py
+++ src/rolch/distributions/gamma.py
@@ -2,18 +2,38 @@ import numpy as np
 import scipy.special as spc
 import scipy.stats as st
 
-from rolch.abc import Distribution
+from rolch.abc import Distribution, LinkFunction
 from rolch.link import LogLink
 
 
 class DistributionGamma(Distribution):
     """Corresponds to GAMLSS GA() and scipy.stats.gamma()"""
 
-    def __init__(self, loc_link=LogLink(), scale_link=LogLink()):
-        self.n_params = 2
+    def __init__(
+        self, loc_link: LinkFunction = LogLink(), scale_link: LinkFunction = LogLink()
+    ):
+        """The Gamma Distribution for GAMLSS.
+
+        The distribution function is defined as in GAMLSS as:
+        $$
+        f(y|\mu,\sigma)=\frac{y^{(1/\sigma^2-1)}\exp[-y/(\sigma^2 \mu)]}{(\sigma^2 \mu)^{(1/\sigma^2)} \Gamma(1/\sigma^2)}
+        $$
+
+        !!! Note
+            The function is parameterized as GAMLSS' GA() distribution.
+            This parameterization is different to the `scipy.stats.gamma(alpha, loc, scale)` parameterization.
+            We can use `DistributionGamma().gamlss_to_scipy(mu, sigma)` to map the distribution parameters to scipy.
+
+        Args:
+            loc_link (LinkFunction, optional): The link function for $\mu$. Defaults to LogLink().
+            scale_link (LinkFunction, optional): The link function for $\sigma$. Defaults to LogLink().
+        """
         self.loc_link = loc_link
         self.scale_link = scale_link
+        # Set up links as dict
         self.links = {0: self.loc_link, 1: self.scale_link}
+        # Set distribution params
+        self.n_params = 2
         self.corresponding_gamlss = "GA"
         self.scipy_dist = st.gamma
 
@@ -21,6 +41,23 @@ class DistributionGamma(Distribution):
         mu = theta[:, 0]
         sigma = theta[:, 1]
         return mu, sigma
+
+    @staticmethod
+    def gamlss_to_scipy(mu: np.ndarray, sigma: np.ndarray):
+        """Map GAMLSS Parameters to scipy parameters.
+
+        Args:
+            mu (np.ndarray): mu parameter
+            sigma (np.ndarray): sigma parameter
+
+        Returns:
+            tuple: Tuple of (alpha, loc, scale) for scipy.stats.gamma(alpha, loc, scale)
+        """
+        alpha = 1 / sigma**2
+        beta = 1 / (sigma**2 * mu)
+        loc = 0
+        scale = 1 / beta
+        return alpha, loc, scale
 
     def dl1_dp1(self, y, theta, param=0):
         mu, sigma = self.theta_to_params(theta)
@@ -68,19 +105,22 @@ class DistributionGamma(Distribution):
         if param == 1:
             return np.ones_like(y)
 
-    # TODO: This can be factored to the base class
     def cdf(self, y, theta):
         mu, sigma = self.theta_to_params(theta)
-        return self.scipy_dist(mu, sigma).cdf(y)
+        return self.scipy_dist(**self.gamlss_to_scipy(mu, sigma)).cdf(y)
 
     def pdf(self, y, theta):
         mu, sigma = self.theta_to_params(theta)
-        return self.scipy_dist(mu, sigma).pdf(y)
+        return self.scipy_dist(**self.gamlss_to_scipy(mu, sigma)).pdf(y)
 
     def ppf(self, q, theta):
         mu, sigma = self.theta_to_params(theta)
-        return self.scipy_dist(mu, sigma).ppf(q)
+        return self.scipy_dist(**self.gamlss_to_scipy(mu, sigma)).ppf(q)
 
     def rvs(self, size, theta):
         mu, sigma = self.theta_to_params(theta)
-        return self.scipy_dist(mu, sigma).rvs((size, theta.shape[0])).T
+        return (
+            self.scipy_dist(**self.gamlss_to_scipy(mu, sigma))
+            .rvs((size, theta.shape[0]))
+            .T
+        )
