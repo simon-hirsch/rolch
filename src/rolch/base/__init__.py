ab70d66cae759d169625b38d7fec968019e2bd17
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-01-01 17:32:25 +0100

    Add base class for estimation methods

--- src/rolch/base/__init__.py
+++ src/rolch/base/__init__.py
@@ -1,4 +1,5 @@
 from .distribution import Distribution
+from .estimation_method import EstimationMethod
 from .link import LinkFunction
 
-__all__ = ["Distribution", "LinkFunction"]
+__all__ = ["Distribution", "LinkFunction", "EstimationMethod"]
