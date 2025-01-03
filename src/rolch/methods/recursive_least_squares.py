f0378c4ab669688fead2aa8b5861f849534400eb
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-01-03 13:16:31 +0100

    Add correct squeeze and type hints

--- src/rolch/methods/recursive_least_squares.py
+++ src/rolch/methods/recursive_least_squares.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+import numpy as np
+
 from rolch.base import EstimationMethod
 from rolch.gram import (
     init_inverted_gram,
@@ -9,7 +13,7 @@ from rolch.gram import (
 
 class OrdinaryLeastSquaresMethod(EstimationMethod):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(
             _path_based_method=False,
             _accepts_bounds=False,
@@ -17,29 +21,58 @@ class OrdinaryLeastSquaresMethod(EstimationMethod):
         )
 
     @staticmethod
-    def init_x_gram(X, weights, forget):
+    def init_x_gram(X: np.ndarray, weights: np.ndarray, forget: float) -> np.ndarray:
         return init_inverted_gram(X, w=weights, forget=forget)
 
     @staticmethod
-    def init_y_gram(X, y, weights, forget):
+    def init_y_gram(
+        X: np.ndarray, y: np.ndarray, weights: np.ndarray, forget: float
+    ) -> np.ndarray:
         return init_y_gram(X, y, w=weights, forget=forget)
 
     @staticmethod
-    def update_x_gram(gram, X, weights, forget):
+    def update_x_gram(
+        gram: np.ndarray, X: np.ndarray, weights: np.ndarray, forget: float
+    ) -> np.ndarray:
         return update_inverted_gram(gram, X, w=weights, forget=forget)
 
     @staticmethod
-    def update_y_gram(gram, X, y, weights, forget):
+    def update_y_gram(
+        gram: np.ndarray,
+        X: np.ndarray,
+        y: np.ndarray,
+        weights: np.ndarray,
+        forget: float,
+    ) -> np.ndarray:
         return update_y_gram(gram, X, y, forget=forget, w=weights)
 
-    def fit_beta_path(self, x_gram, y_gram, is_regularized):
+    def fit_beta_path(
+        self, x_gram: np.ndarray, y_gram: np.ndarray, is_regularized: np.ndarray
+    ) -> np.ndarray:
         return super().fit_beta_path(x_gram, y_gram, is_regularized)
 
-    def update_beta_path(self, x_gram, y_gram, beta_path, is_regularized):
+    def update_beta_path(
+        self,
+        x_gram: np.ndarray,
+        y_gram: np.ndarray,
+        beta_path: np.ndarray,
+        is_regularized: np.ndarray,
+    ) -> np.ndarray:
         return super().update_beta_path(x_gram, y_gram, beta_path, is_regularized)
 
-    def fit_beta(self, x_gram, y_gram, is_regularized=None):
-        return (x_gram @ y_gram).squeeze()
+    def fit_beta(
+        self,
+        x_gram: np.ndarray,
+        y_gram: np.ndarray,
+        is_regularized: Optional[np.ndarray] = None,
+    ) -> np.ndarray:
+        return (x_gram @ y_gram).squeeze(-1)
 
-    def update_beta(self, x_gram, y_gram, beta, is_regularized=None):
-        return (x_gram @ y_gram).squeeze()
+    def update_beta(
+        self,
+        x_gram: np.ndarray,
+        y_gram: np.ndarray,
+        beta: np.ndarray,
+        is_regularized: Optional[np.ndarray] = None,
+    ) -> np.ndarray:
+        return (x_gram @ y_gram).squeeze(-1)
