eb7678020dc94bb4fceb0da5fef3b7e22710136f
Jonathan Berrisch <Jonathan@Berrisch.biz>
2025-04-25 16:37:24 +0200

    Remove redundant export

--- src/rolch/link/__init__.py
+++ src/rolch/link/__init__.py
@@ -16,7 +16,6 @@ __all__ = [
     "SqrtLink",
     "SqrtShiftValueLink",
     "SqrtShiftTwoLink",
-    "SqrtShiftTwoLink",
     "InverseSoftPlusLink",
     "InverseSoftPlusShiftValueLink",
     "InverseSoftPlusShiftTwoLink",
