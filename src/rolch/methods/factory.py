2641ec9027e38c5244e3697416e224e5ce118786
Simon Hirsch <64348015+simon-hirsch@users.noreply.github.com>
2025-04-29 10:00:06 +0200

    Update factory.py

--- src/rolch/methods/factory.py
+++ src/rolch/methods/factory.py
@@ -21,7 +21,7 @@ class EstimationMethodFactory:
             print("Setting default alpha to 0.5.")
             return ElasticNetPathMethod(alpha=0.5)
         else:
-            return ValueError("Did not recognize method. Please provide [ols, lasso].")
+            raise ValueError("Did not recognize method. Please provide [ols, lasso, "elasticnet].")
 
 
 def get_estimation_method(method: EstimationMethod | str):
