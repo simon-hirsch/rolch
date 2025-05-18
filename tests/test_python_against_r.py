2c882171b83ac0d89f170bf9b9b3eabb7b030d57
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-05-18 09:10:25 +0200

    Put data into tests/data instead of referencing some other repo

--- tests/test_python_against_r.py
+++ tests/test_python_against_r.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from rolch import DistributionNormal, OnlineGamlss
 
-file = "https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv"
+file = "tests/data/mtcars.csv"
 mtcars = np.genfromtxt(file, delimiter=",", skip_header=1)[:, 1:]
 
 y = mtcars[:, 0]
