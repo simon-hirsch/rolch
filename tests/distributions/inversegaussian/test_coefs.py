911ebdc1b51d677de055704ddd5164406c7c0ad4
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-06-04 19:51:51 +0200

    Add test on MT-cars

---
+++ tests/distributions/inversegaussian/test_coefs.py
@@ -0,0 +1,53 @@
+import numpy as np
+import rpy2.robjects as robjects
+
+from ondil import DistributionInverseGaussian, OnlineGamlss
+
+file = "tests/data/mtcars.csv"
+mtcars = np.genfromtxt(file, delimiter=",", skip_header=1)[:, 1:]
+
+y = mtcars[:, 0]
+X = mtcars[:, 1:]
+
+
+def test_inversegaussian_distribution():
+    dist = DistributionInverseGaussian()
+
+    code = f"""
+    library("gamlss")
+    data(mtcars)
+
+    model = gamlss(
+        mpg ~ cyl + hp,
+        sigma.formula = ~cyl + hp,
+        family=gamlss.dist::{dist.corresponding_gamlss}(),
+        data=as.data.frame(mtcars)
+    )
+
+    list(
+        "mu" = coef(model, "mu"),
+        "sigma" = coef(model, "sigma")
+    )
+    """
+
+    R_list = robjects.r(code)
+    coef_R_mu = np.array(R_list.rx2("mu"))
+    coef_R_sg = np.array(R_list.rx2("sigma"))
+
+    estimator = OnlineGamlss(
+        distribution=dist,
+        equation={0: np.array([0, 2]), 1: np.array([0, 2])},
+        method="ols",
+        scale_inputs=False,
+        fit_intercept=True,
+        rss_tol_inner=10,
+    )
+
+    estimator.fit(X=X, y=y)
+
+    assert np.allclose(
+        estimator.beta[0], coef_R_mu, atol=0.01
+    ), "Location coefficients don't match"
+    assert np.allclose(
+        estimator.beta[1], coef_R_sg, atol=0.01
+    ), "Scale coefficients don't match"
