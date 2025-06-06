ae82f503196b5e121dcbee94d4746f761f4dfece
murthy-econometrics-5819 <120830181+murthy-econometrics-5819@users.noreply.github.com>
2025-06-06 12:57:32 +0200

    added test for coefs

---
+++ tests/distributions/betainflated/test_coefs.py
@@ -0,0 +1,88 @@
+import numpy as np 
+import rpy2.robjects as robjects 
+from ondil import DistributionBetaInflated, OnlineGamlss
+
+def test_beta_distribution(): 
+    dist = DistributionBetaInflated 
+
+    code = f"""
+    library(gamlss)
+    set.seed(1)
+    n <- 2000
+    mu <- runif(1)
+    sigma <- runif(1)
+    nu <- runif(1)
+    tau <- runif(1)
+
+
+    y <- gamlss.dist::rBEINF(n, mu, sigma, nu, tau)
+    x1 <- rnorm(n)
+    x2 <- rnorm(n)
+
+    model <- gamlss(
+    y ~ x1 + x2,
+    sigma.formula = ~x1 + x2,
+    nu.formula = ~x1 + x2,
+    tau.formula = ~x1 + x2,
+    family = BEINF()
+    )
+
+    list(
+    "y" = y,
+    "x1" = x1, 
+    "x2" = x2,
+    "mu" = mu,
+    "sigma" = sigma,
+    "nu" = nu,
+    "tau" = tau,
+    "coef_R_mu" = coef(model, "mu"),
+    "coef_R_sg" = coef(model, "sigma"),
+    "coef_R_nu" = coef(model, "nu"),
+    "coef_R_tau" = coef(model, "tau")    
+    )
+    """
+
+    #obtain info from R
+
+    R_list = robjects.r(code)
+    y = np.array(R_list.rx2("y"))
+    x1 = np.array(R_list.rx2("x1"))
+    x2 = np.array(R_list.rx2("x2"))
+    X = np.column_stack((x1, x2))
+
+    theta = np.array(
+        [
+            R_list.rx2("mu"),  # mu
+            R_list.rx2("sigma"),  # sigma
+            R_list.rx2("nu"), #nu 
+            R_list.rx2("tau"), #tau
+        ]
+    )
+
+    estimator = OnlineGamlss(
+        distribution=DistributionBetaInflated(),
+        equation={0: 'all', 1: 'all', 2: 'all', 3:'all'},
+        method="ols",
+        scale_inputs=False,
+        fit_intercept=True,
+        rss_tol_inner=10,
+    )
+
+    estimator.fit(X=X, y=y)
+    print("Difference in estimates: ",estimator.beta[0] - R_list.rx2("coef_R_mu"))
+    assert np.allclose(estimator.beta[0], R_list.rx2("coef_R_mu")), (
+        "Location coefficients don't match"
+    )
+    print("Difference in estimates: ", estimator.beta[1] - R_list.rx2("coef_R_sg"))
+    assert np.allclose(estimator.beta[1], R_list.rx2("coef_R_sg")), (
+        "Scale coefficients don't match"
+    )
+    print("Difference in estimates: ",estimator.beta[2] - R_list.rx2("coef_R_nu"))
+    assert np.allclose(estimator.beta[2], R_list.rx2("coef_R_nu")), (
+        "Skew coefficients don't match"
+    )
+    print("Difference in estimates: ", estimator.beta[3] - R_list.rx2("coef_R_tau"))
+    assert np.allclose(estimator.beta[3], R_list.rx2("coef_R_tau")), (
+        "Kurtosis coefficients don't match"
+    )
+
