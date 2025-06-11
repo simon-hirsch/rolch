e24db1f9c5f52cc618079a8c10fe132a5ff233e3
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-06-11 16:34:54 +0200

    test for sklearn compliance for the online linear model

---
+++ tests/test_sklearn.py
@@ -0,0 +1,26 @@
+import pytest
+from sklearn.utils.estimator_checks import check_estimator
+
+import ondil
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.utils.estimator_checks.check_estimator.html#sklearn.utils.estimator_checks.check_estimator
+
+
+@pytest.mark.parametrize("scale_inputs", [False])
+@pytest.mark.parametrize("fit_intercept", [False, True])
+@pytest.mark.parametrize("method", ["ols", "lasso", "elasticnet"])
+@pytest.mark.parametrize("ic", ["aic", "bic", "hqc", "max"])
+def test_sklearn_compliance_linear_model(scale_inputs, fit_intercept, method, ic):
+    estimator = ondil.OnlineLinearModel(
+        fit_intercept=fit_intercept,
+        scale_inputs=scale_inputs,
+        method=method,
+        ic=ic,
+    )
+    check_estimator(
+        estimator,
+        on_fail="raise",
+        expected_failed_checks={
+            "check_sample_weight_equivalence_on_dense_data": "To few data points to test this check in the original sklaern implementation.",
+        },
+    )
