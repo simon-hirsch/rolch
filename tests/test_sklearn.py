d289f8ae3e9e615ce134ecb30d8593228c8d43d5
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-06-15 13:29:44 +0200

    Make the online gamlss sklearn compliant

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -3,6 +3,10 @@ from sklearn.utils.estimator_checks import check_estimator
 
 import ondil
 
+EXPECTED_FAILED_CHECKS = {
+    "check_sample_weight_equivalence_on_dense_data": "To few data points to test this check in the original sklaern implementation.",
+}
+
 # https://scikit-learn.org/stable/modules/generated/sklearn.utils.estimator_checks.check_estimator.html#sklearn.utils.estimator_checks.check_estimator
 
 
@@ -18,11 +22,21 @@ def test_sklearn_compliance_linear_model(scale_inputs, fit_intercept, method, ic
         ic=ic,
     )
     check_estimator(
-        estimator,
-        on_fail="raise",
-        expected_failed_checks={
-            "check_sample_weight_equivalence_on_dense_data": "To few data points to test this check in the original sklaern implementation.",
-        },
+        estimator, on_fail="raise", expected_failed_checks=EXPECTED_FAILED_CHECKS
+    )
+
+
+@pytest.mark.parametrize("scale_inputs", [False, True])
+@pytest.mark.parametrize("method", ["ols", "lasso", "elasticnet"])
+@pytest.mark.parametrize("ic", ["aic", "bic", "hqc", "max"])
+def test_sklearn_compliance_online_gamlss(scale_inputs, method, ic):
+    estimator = ondil.OnlineGamlss(
+        scale_inputs=scale_inputs,
+        method=method,
+        ic=ic,
+    )
+    check_estimator(
+        estimator, on_fail="raise", expected_failed_checks=EXPECTED_FAILED_CHECKS
     )
 
 
