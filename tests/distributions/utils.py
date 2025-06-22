ddcc181a84553a9d7cedc8ecd74ee6ae8b259d19
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-06-22 18:13:37 +0200

    Ensure we only import those where corresponding_gamlss is not none

--- tests/distributions/utils.py
+++ tests/distributions/utils.py
@@ -1,4 +1,5 @@
 import inspect
+
 import ondil.distributions
 
 
@@ -6,7 +7,9 @@ def get_distributions_with_gamlss():
     """Get all distribution classes that have a corresponding_gamlss attribute."""
     distributions = []
     for name, obj in inspect.getmembers(ondil.distributions):
-        if hasattr(obj, "corresponding_gamlss"):
+        if hasattr(obj, "corresponding_gamlss") and (
+            obj.corresponding_gamlss is not None
+        ):
             instance = obj()
             if instance.corresponding_gamlss is not None:
                 distributions.append(instance)
